086904f2f717149ecc5310c3e004b34d434db2c3
Belal123-dell <basselessam1227@gmail.com>
2025-03-08 17:10:16 +0200

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,87 @@
+
+
+export const HERO_CONTENT = 
+`I am a senior-2 student at faculty of Engineering, Ain Shams University. I am passionate about web development and software engineering.
+I am interested in building a softwares and I am in the software product line track in my college. I started with learning the basics of the web development
+which is HTML, CSS, and JavaScript. Then I enrolled in many trainings about the software development one of them web-based apps and then the mobile app using flutter.
+Then I started to be more focused in the backend development and I started to learn the Node.js and Express.js and MongoDB, then I have learned many design patterns.
+After all of this i have started learning backend development usin php and laravel in order to know many technologies and to be more flexible in the software engineering field.
+ I am always eager to learn new technologies and improve my skills and that will be appeared in the experience and projects section.
+ I am looking for learning to many things in the software engineering field and I am looking for a job opportunity to gain experience and apply my knowledge.`;
+
+
+export const EXPERIENCES = [ 
+  {
+    year: "October 2020 – December 2020",
+    role: "Arduino Trainee",
+    company: "221B Co-working Space",
+    description: ` This course discuss the fundamental of c/c++ programming and using them to make a functional project by arduino uno.
+                   The final project of this course is program that uploaded on the uno that will make a small car park inside a corner.`,
+    technologies: ["C/C++", "Arduino"],
+  },
+  {
+    year: "August 2022 – September 2022",
+    role: "AI trainee",
+    company: "Telecom Egypt",
+    description: `The AI training course covers an overview of artificial intelligence, Python and data science libraries, machine learning
+                  and deep learning fundamentals, convolutional neural networks (CNNs), building CNN architectures for custom data,
+                  object detection and segmentation, using pre-trained Mask-RCNN models for computer vision, labeling and annotating
+                  custom datasets, and extracting frames from videos with automated tools.`,
+    technologies: ["Python","Postgres","Tensorflow","Keras"],
+  },
+  {
+    year: "October 2022 – January 2023",
+    role: "Full Stack Development trainee",
+    company: "Sprints",
+    description: ` This course covers the basics of web development and the most popular technologies in the field like HTML, CSS, JavaScript
+                   The course also learned me the frontend usin Angular and the backend development using Node.js and Express.js and according to the part of the databases using MongoDB
+                   After that we learn to use the framework Angular in the frontend then we start in implementing the final project which is
+                  e-commerce.`,
+    technologies: ["Html", "CSS", "JavaScript", "NodeJs", "ExpressJs", "MongoDB", "Angular"],
+  },
+  {
+    year: "July 2023 – October 2023",
+    role: "Flutter Development trainee",
+    company: "Sprints",
+    description: ` This course starts with covering the basics of mobile development and the most popular technologies in the field like Dart, Flutter
+                   The course also learned me the frontend using Flutter and the backend development using Firebase
+                   After that we learn to use the framework Flutter in the frontend then we start in implementing the final project which is
+                  a mobile application for a restaurant and i have implment many other projects using flutter like Headiaty which is a gift management system`,
+    technologies: ["Dart", "Fultter", "Firebase", "Sqlite"],
+  },
+
+  {
+    year: "April 2024 – June 2024",
+    role: "Cloud Computing trainee",
+    company: "YouTube channel",
+    description: `This a project is such a challenge for me. The main idea of the project is to make image processing for a certain images
+you will upload it from the local host then by using amazon ec2 you will create three instances one of them is the master
+and the other two are the slaves then you will distribute the images on them and work on it then return it to the master
+then will be downloaded on the local host.`,
+    technologies: ["Python", "AWS", "Linux", "Docker"],
+  },
+
+  {
+    year: "June 2024 – October 2024",
+    role: "CyberOps trainee",
+    company: "NTI",
+    description: `This is a training about the CyberOps and the main idea of this training is to learn the basics of the cyber security and how to protect the network from the attacks and how to detect the attacks and how to respond to it.
+    The training covers the following topics:
+    • Introduction to CyberOps
+    • Network Security
+    • Security Monitoring
+    • Host-Based Analysis
+    • Security Policies and Procedures
+    • Incident Response
+    `,
+  },
+
+];
+
+
+
+export const CONTACT = {
+  address: "3 el kamal street, Masr el gdeda, Cairo, Egypt",
+  phoneNo: "01027117001 ",
+  email: "belalgazia@gmail.com",
+};
